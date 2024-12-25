106eabbc1a3af1df2096cd5533f3b89806de57fb
Coderyounes <younesanas2021@gmail.com>
2024-12-25 20:53:16 +0100

    FEAT: integration d'une Code SQl pour definir notre schema  #1

--- sqlCode/schema.sql
+++ sqlCode/schema.sql
@@ -6,52 +6,69 @@ CREATE DATABASE IF NOT EXISTS GeoJuniorV2;
 USE GeoJuniorV2;
 
 
-CREATE Users IF NOT EXISTS (
-    Id_user int NOT NULL AUTO_INCREMENT,
-    Nom varchar(255) NOT NULL,
-    Prenom varchar(255) NOT NULL,
-    Email varchar(255) NOT NULL,
-    Password varchar(255) NOT NULL,
-    Role int NOT NULL,
-    Token varchar(255) NULL,
-    PRIMARY KEY(Id_user)
-);
 
-CREATE Roles IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Roles (
     Id_role int NOT NULL AUTO_INCREMENT,
     Role_name varchar(20),
     PRIMARY KEY(Id_role)
 );
 
+CREATE TABLE IF NOT EXISTS Users(
+    Id_user int NOT NULL AUTO_INCREMENT,
+    Nom varchar(255) NOT NULL,
+    Prenom varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL,
+    Password varchar(255) NOT NULL,
+    Id_role int NOT NULL,
+    Token varchar(255) NULL,
+    PRIMARY KEY(Id_user),
+    FOREIGN KEY(id_role) REFERENCES Roles(Id_role)
+    
+);
 
-CREATE Continents IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Continents (
     Id_continent int NOT NULL AUTO_INCREMENT,
     Continent_name varchar(255) NOT NULL UNIQUE,
     Count_pays int default 0,
-    PRIMARY KEY(Id_continent)
+    Created_by int NOT NULL, 
+    PRIMARY KEY(Id_continent),
+    FOREIGN KEY(Created_by) REFERENCES Users(Id_user)
 );
 
-CREATE Langues IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Langues (
     Id_langue int NOT NULL AUTO_INCREMENT,
     Langue_nom varchar(255) NOT NULL,
     PRIMARY KEY(Id_langue)
 );
 
-CREATE Pays IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Pays (
     Id_pays int NOT NULL AUTO_INCREMENT,
     Nom_pays varchar(255) NOT NULL UNIQUE,
     Population int NOT NULL,
     Id_continent int NOT NULL,
+    Created_by int NOT NULL,
     PRIMARY KEY(Id_pays),
-    FOREIGN KEY (Id_continent) REFERENCES Continents(Id_continent)
+    FOREIGN KEY (Id_continent) REFERENCES Continents(Id_continent),
+    FOREIGN KEY(Created_by) REFERENCES Users(Id_user)
 );
 
-CREATE Villes IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Pays_Langues (
+    Id int NOT NULL,
+    Id_pays int NOT NULL,
+    Id_langue int NOT NULL,
+    PRIMARY KEY(Id),
+    FOREIGN KEY (Id_pays) REFERENCES Pays(Id_pays),
+    FOREIGN KEY (Id_langue) REFERENCES Langues(Id_langue)
+);
+
+CREATE TABLE IF NOT EXISTS Villes (
     Id_ville int NOT NULL AUTO_INCREMENT,
     Nom_ville varchar(255),
     Description_ville varchar(255),
     Type_Ville ENUM('Capital', 'Autre'),
     Id_pays int NOT NULL,
+    Created_by int NOT NULL,
     PRIMARY KEY(Id_ville),
-    FOREIGN KEY (Id_pays) REFERENCES Pays(Id_pays)
+    FOREIGN KEY (Id_pays) REFERENCES Pays(Id_pays),
+    FOREIGN KEY(Created_by) REFERENCES Users(Id_user)
 );

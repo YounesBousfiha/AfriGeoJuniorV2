e4db89cb93ecb1e6f3497b4c4634e0ccda7e7a68
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-27 22:45:16 +0100

    Ajouter la fonctionnalité de récupération et d'affichage de tous les continents dans la liste des continents

--- src/pages/continents-list.php
+++ src/pages/continents-list.php
@@ -1,3 +1,12 @@
+<?php
+include "../../config/db.php";
+include "../../controllers/adminController.php";
+
+$db = new DBConnection();
+$admin = new AdminController($db);
+$all_continents = $admin->getAllContinent();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -106,13 +115,13 @@
                         </thead>
                         <tbody>
                             <?php
-                            for ($i = 0; $i < 22; $i++) {
+                            foreach ($all_continents as $continent) {
                                 echo "<tr class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
                                 <th class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
-                                43
+                                $continent[Id_continent]
                                 </th>
                                 <td class='px-6 py-2'>
-                                Africa
+                                $continent[Continent_name]
                                 </td>
                                 <td class='px-6 py-2 flex gap-2'>
                                         <a href='../../src/pages/cruds/continent/update.php?id=12' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>

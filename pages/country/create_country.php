c7343138c68ddbb77027ce2be286456d4539a710
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-27 22:45:08 +0100

    Améliorer la validation des utilisateurs administrateurs lors de la création de pays et simplifier le traitement des données du formulaire

--- pages/country/create_country.php
+++ pages/country/create_country.php
@@ -6,43 +6,43 @@ include '../../model/PaysModel.php';
 include '../../controllers/adminController.php';
 
 $db = new DBConnection();
+session_start();
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // validate if it an admin if not admin  redirect it to home page
+// validate if it an admin if not admin  redirect it to home page
+$pays_nom = Helpers::validateData($_SESSION["pays-nom"]);
+$pays_population = Helpers::validateData($_SESSION['pays-population']);
+$id_continent = Helpers::validateData($_SESSION["id-continent"]);
+$pays_image = Helpers::validateData($_SESSION['pays-image']);
+if ($_COOKIE['auth_token']) {
+    $person = new PersonController($db);
+    $user = $person->validateUser();
 
-    $pays_nom = Helpers::validateData($_POST['pays_nom']);
-    $pays_population = Helpers::validateData($_POST['pays_population']);
-    $id_continent = 2;
-    $pays_image = Helpers::validateData($_POST['pays_image']);
-    if($_COOKIE['auth_token']) {
-        $person = new PersonController($db);
-        $user = $person->validateUser();
-
-        if($user['Id_role'] != 1) {
-            echo "You are not an admin";
-            header("Location: /GoAway");
-        }
-
-        $pays_nom = Helpers::validateData($_POST['pays_nom']);
-        $pays_population = Helpers::validateData($_POST['pays_population']);
-        $id_continent = Helpers::validateData($_POST['id_continent']);
-        $pays_image = Helpers::validateData($_POST['pays_image']);
-        $created_by = $user['Id_user'];
-
-        var_dump($pays_nom, $pays_population, $id_continent, $created_by);
-
-        if($pays_nom && $pays_population && $id_continent && $created_by) {
-       
-            $newCountry = new Pays($pays_nom, $pays_population, $pays_image, $id_continent, $created_by);
-    
-            $admin = new AdminController($db, null, $newCountry, null);
-            $admin->createPays($newCountry);
-        } else {
-            echo "Invalidate Data";
-        }
-    } else {
-        echo "You are not logged in";
+    if ($user['Id_role'] != 1) {
+        echo "You are not an admin";
+        header("Location: /GoAway");
     }
+
+    $pays_nom = Helpers::validateData($_SESSION['pays-nom']);
+    $pays_population = Helpers::validateData($_SESSION['pays-population']);
+    $id_continent = Helpers::validateData($_SESSION['id-continent']);
+    $pays_image = Helpers::validateData($_SESSION['pays-image']);
+    $created_by = $user['Id_user'];
+
+    var_dump($user["Id_user"]);
+
+    if ($pays_nom && $pays_population && $id_continent && $created_by) {
+
+        $newCountry = new Pays($pays_nom, $pays_population, $pays_image, $id_continent, $created_by);
+
+        $admin = new AdminController($db, null, $newCountry, null);
+        $admin->createPays($newCountry);
+        session_unset();
+        header("location: ./../../src/pages/countries-list.php");
+    } else {
+        echo "Invalidate Data";
+    }
+} else {
+    echo "You are not logged in";
 }
 
 
@@ -51,5 +51,3 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
 // Create Pages for CRUDS for Continent , Ville , Pays
-?>
-

d5e5573f94fd47b51a06b306bcad6d119038e480
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-28 14:01:57 +0100

    Refactorisez update_country.php et update_ville.php pour utiliser les données de session pour les mises à jour de pays et de ville, améliorant ainsi la sécurité et la clarté du code

--- pages/country/update_country.php
+++ pages/country/update_country.php
@@ -6,61 +6,41 @@ include '../../model/PaysModel.php';
 include '../../controllers/adminController.php';
 
 $db = new DBConnection();
+session_start();
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // validate if it an admin if not admin  redirect it to home page
+$pays_id = Helpers::validateData($_SESSION["pays-id"]);
+$pays_nom = Helpers::validateData($_SESSION["pays-nom"]);
+$pays_population = Helpers::validateData($_SESSION["pays-population"]);
+$id_continent = Helpers::validateData($_SESSION["id-continent"]);
+$pays_image = Helpers::validateData($_SESSION["pays-image"]);
 
-    $pays_id = Helpers::validateData($_POST['pays_id']);
-    $pays_nom = Helpers::validateData($_POST['pays_nom']);
-    $pays_population = Helpers::validateData($_POST['pays_population']);
-    $id_continent = Helpers::validateData($_POST['id_continent']);
-    $pays_image = Helpers::validateData($_POST['pays_image']);
+if ($_COOKIE['auth_token']) {
+    $person = new PersonController($db);
+    $user = $person->validateUser();
 
-    if($_COOKIE['auth_token']) {
-        $person = new PersonController($db);
-        $user = $person->validateUser();
-
-        if($user['Id_role'] != 1) {
-            echo "You are not an admin";
-            header("Location: /GoAway");
-        }
-
-        if($pays_id && $pays_nom && $pays_population && $id_continent) {
-            $updatedCountry = new Pays($pays_nom, $pays_population, $pays_image, $id_continent, $user['Id_user']);
-            $updatedCountry->__set('pays_id', $pays_id);
-            $updatedCountry->__set('pays_nom', $pays_nom);
-            $updatedCountry->__set('pays_population', $pays_population);
-            $updatedCountry->__set('id_continent', $id_continent);
-            $updatedCountry->__set('pays_image', $pays_image);
-
-            $admin = new AdminController($db, null, $updatedCountry, null);
-
-            $admin->updatePays($updatedCountry->__get('pays_id'), $updatedCountry->__get('pays_nom'), $updatedCountry->__get('pays_population'), $updatedCountry->__get('id_continent'), $updatedCountry->__get('pays_image'));
-        } else {
-            echo "Invalidate Data";
-        }
-    } else {
-        echo "You are not logged in";
+    if ($user['Id_role'] != 1) {
+        echo "You are not an admin";
+        header("Location: /GoAway");
     }
+
+    if ($pays_id && $pays_nom && $pays_population && $id_continent) {
+        $updatedCountry = new Pays($pays_nom, $pays_population, $pays_image, $id_continent, $user['Id_user']);
+        $updatedCountry->__set('pays_id', $pays_id);
+        $updatedCountry->__set('pays_nom', $pays_nom);
+        $updatedCountry->__set('pays_population', $pays_population);
+        $updatedCountry->__set('id_continent', $id_continent);
+        $updatedCountry->__set('pays_image', $pays_image);
+
+        var_dump($updatedCountry);
+
+        $admin = new AdminController($db, null, $updatedCountry, null);
+
+        $admin->updatePays($updatedCountry->__get('pays_id'), $updatedCountry->__get('pays_nom'), $updatedCountry->__get('pays_population'), $updatedCountry->__get('id_continent'), $updatedCountry->__get('pays_image'));
+
+        header("location: ./../../src/pages/countries-list.php");
+    } else {
+        echo "Invalidate Data";
+    }
+} else {
+    echo "You are not logged in";
 }
-?>
-
-
-<form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
-    <label for="pays_id">Country ID:</label>
-    <input type="text" id="pays_id" name="pays_id" required><br>
-
-    <label for="pays_nom">Country Name:</label>
-    <input type="text" id="pays_nom" name="pays_nom" required><br>
-
-    <label for="pays_population">Population:</label>
-    <input type="text" id="pays_population" name="pays_population" required><br>
-
-    <label for="id_continent">Continent ID:</label>
-    <input type="text" id="id_continent" name="id_continent" required><br>
-
-    <label for="pays_image">Image URL:</label>
-    <input type="text" id="pays_image" name="pays_image"><br>
-
-    <input type="submit" value="Update Country">
-</form>
--- pages/ville/update_ville.php
+++ pages/ville/update_ville.php
@@ -5,63 +5,40 @@ include '../../helpers/helpers.php';
 include '../../model/VilleModel.php';
 include '../../controllers/adminController.php';
 
-
 $db = new DBConnection();
+session_start();
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // validate if it an admin if not admin  redirect it to home page
+$ville_id = Helpers::validateData($_SESSION["city-id"]);
+$ville_nom = Helpers::validateData($_SESSION["city-nom"]);
+$id_pays = Helpers::validateData($_SESSION["id-pays"]);
+$ville_image = Helpers::validateData($_SESSION["city-image"]);
+$ville_type = Helpers::validateData($_SESSION["city-type"]);
+$ville_description = "lorem ipsum";
 
-    $ville_id = Helpers::validateData($_POST['ville_id']);
-    $ville_nom = Helpers::validateData($_POST['ville_nom']);
-    $id_pays = Helpers::validateData($_POST['id_pays']);
-    $ville_image = Helpers::validateData($_POST['ville_image']);
-    $ville_type = Helpers::validateData($_POST['ville_type']);
-    $ville_description = Helpers::validateData($_POST['ville_description']);
+if (isset($_COOKIE['auth_token'])) {
+    $person = new PersonController($db);
+    $user = $person->validateUser();
 
-    if($_COOKIE['auth_token']) {
-        $person = new PersonController($db);
-        $user = $person->validateUser();
-
-        if($user['Id_role'] != 1) {
-            echo "You are not an admin";
-            header("Location: /GoAway");
-        }
-
-        if($ville_id && $ville_nom && $ville_description && $id_pays) {
-            $updatedCity = new Ville($ville_nom, $ville_description, $ville_type, $ville_image, $id_pays, $user['Id_user']);
-            $updatedCity->__set('ville_id', $ville_id);
-            $updatedCity->__set('ville_nom', $ville_nom);
-            $updatedCity->__set('id_pays', $id_pays);
-            $updatedCity->__set('ville_image', $ville_image);
-            $updatedCity->__set('ville_description', $ville_description);
-
-            $admin = new AdminController($db, null, null, $updatedCity);
-            var_dump($updatedCity->__get('ville_id'));
-            var_dump($updatedCity->__get('ville_nom'));
-            var_dump($updatedCity->__get('ville_description'));
-
-            $admin->updateVille($updatedCity->__get('ville_id'), $updatedCity->__get('ville_nom'), $updatedCity->__get('ville_description'));
-        } else {
-            echo "Invalidate Data";
-        }
-    } else {
-        echo "You are not logged in";
+    if ($user['Id_role'] != 1) {
+        echo "You are not an admin";
+        header("Location: /GoAway");
     }
+
+    if ($ville_id && $ville_nom && $ville_description && $id_pays) {
+        $updatedCity = new Ville($ville_nom, $ville_description, $ville_type, $ville_image, $id_pays, $user['Id_user']);
+        $updatedCity->__set('ville_id', $ville_id);
+        $updatedCity->__set('ville_nom', $ville_nom);
+        $updatedCity->__set('id_pays', $id_pays);
+        $updatedCity->__set('ville_image', $ville_image);
+        $updatedCity->__set('ville_description', $ville_description);
+
+        $admin = new AdminController($db, null, null, $updatedCity);
+
+        $admin->updateVille($updatedCity->__get('ville_id'), $updatedCity->__get('ville_nom'), $updatedCity->__get('ville_description'));
+        header("location: ./../../src/pages/cities-list.php");
+    } else {
+        echo "Invalidate Data";
+    }
+} else {
+    echo "You are not logged in";
 }
-?>
-
-<form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
-    <label for="ville_id">City ID:</label>
-    <input type="text" id="ville_id" name="ville_id" required><br>
-
-    <label for="ville_nom">City Name:</label>
-    <input type="text" id="ville_nom" name="ville_nom" required><br>
-
-    <label for="id_pays">Country ID:</label>
-    <input type="text" id="id_pays" name="id_pays" required><br>
-
-    <label for="ville_description">Description:</label>
-    <input type="text" id="ville_description" name="ville_description"><br>
-
-    <input type="submit" value="Update City">
-</form>

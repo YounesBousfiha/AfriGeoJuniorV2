8ea118cc6646a32a7bc73359b8775d464873c6a0
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-28 09:58:45 +0100

    Ajouter le fonctionemment d'ajouter une ville avec la gestion des erreurs d'utilisateur lors de l'insertion

--- src/pages/cruds/city/create.php
+++ src/pages/cruds/city/create.php
@@ -10,23 +10,23 @@ $id_pays_err = "";
 $city_image_err = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $pays_nom = $_POST["pays-nom"];
-    $pays_population = $_POST["pays-population"];
-    $id_continent = isset($_POST["id-continent"]) ? $_POST["id-continent"] : null;
-    $pays_image = $_POST["pays-image"];
+    $city_nom = $_POST["pays-nom"];
+    $city_type = isset($_POST["pays-type"]) ? $_POST["pays-type"] : null;
+    $id_pays = isset($_POST["id-continent"]) ? $_POST["id-pays"] : null;
+    $city_image = $_POST["pays-image"];
 
-    if (empty($pays_nom)) $pays_nom_err = "Country name is required...";
-    if (empty($pays_population)) $pays_population_err = "Country population is required...";
-    if (empty($id_continent)) $id_continent_err = "Country continent is required...";
-    if (empty($pays_image)) $pays_image_err = "Country image is required...";
+    if (empty($pays_nom)) $pays_nom_err = "City name is required...";
+    if (empty($city_type)) $city_type_err = "City typr is required...";
+    if (empty($id_continent)) $id_continent_err = "City country is required...";
+    if (empty($pays_image)) $pays_image_err = "City image is required...";
 
-    if (!empty($pays_nom && !empty($pays_population) && !empty($id_continent) && !empty($pays_image))) {
+    if (!empty($city_nom) && !empty($city_type) && !empty($id_pays) && !empty($city_image)) {
         session_start();
-        $_SESSION["pays-nom"] = $pays_nom;
-        $_SESSION["pays-population"] = $pays_population;
-        $_SESSION["id-continent"] = $id_continent;
-        $_SESSION["pays-image"] = $pays_image;
-        header("location: ../../../../pages/country/create_country.php");
+        $_SESSION["city-nom"] = $city_nom;
+        $_SESSION["city-type"] = $city_type;
+        $_SESSION["id-pays"] = $id_pays;
+        $_SESSION["image-image"] = $city_image;
+        header("location: localhost\AfriGeoJuniorV2\pages\ville\create_ville.php");
     }
 }
 
@@ -35,9 +35,7 @@ include '../../../../controllers/AdminController.php';
 
 $db = new DBConnection();
 $admin = new AdminController($db);
-$all_cities = $admin->getAllPays();
-
-var_dump($all_cities);
+$all_countries = $admin->getAllPays();
 ?>
 
 
@@ -70,30 +68,31 @@ var_dump($all_cities);
 
                 <div class="flex-grow flex flex-col text-sm gap-1 mb-4">
                     <label for="city-nom" class="text-gray-600">City name</label>
-                    <input name="city-nom" id="city-nom" type="text" class="bg-gray-100 p-1" placeholder="eg: Meknes, Fes...">
-                    <!-- <p id="city-name-errMssg" class="text-red-600 bg-red-50 px-1 text-sm">This field is Required...</p> -->
+                    <input value="<?= $city_nom ?>" name="city-nom" id="city-nom" type="text" class="bg-gray-100 p-1" placeholder="eg: Meknes, Fes...">
+                    <p id="city-nom-err" class="text-red-600 bg-red-50 px-1 text-sm"><?= $city_nom_err ?></p>
 
                     <label for="city-type" class="text-gray-600">City Type</label>
-                    <select name="city-type" id="city-type" class="bg-gray-100 p-1">
+                    <select value="<?= $city_ty ?>" name="city-type" id="city-type" class="bg-gray-100 p-1">
                         <option value="" selected disabled>Select a Type</option>
                         <option value="Capital">Capital</option>
                         <option value="Autre">Autre</option>
                     </select>
-                    <!-- <p id="city-type-errMssg" class="text-red-600 bg-red-50 px-1 text-sm">This field is Required...</p> -->
+                    <p id="city-type-err" class="text-red-600 bg-red-50 px-1 text-sm"><?= $city_type_err ?></p>
 
-                    <label for="city-country" class="text-gray-600">Country of City</label>
-                    <select name="city-country" id="city-country" class="bg-gray-100 p-1">
+                    <label for="id-pays" class="text-gray-600">Country of City</label>
+                    <select value="<?= $id_pays ?>" name="id-pays" id="id-pays" class="bg-gray-100 p-1">
                         <option value="" selected disabled>Select a Country</option>
                         <?php
-                        foreach($all_cities as $city){
-                            echo "<option value='$citi[Id_pays]'>$city[Nom_pays]</option>";
+                        foreach($all_countries as $country){
+                            echo "<option value='$country[Id_pays]'>$country[Nom_pays]</option>";
                         }
                         ?>
                     </select>
-                    <!-- <p id="city-type-errMssg" class="text-red-600 bg-red-50 px-1 text-sm">This field is Required...</p> -->
+                    <p id="id-pays-err" class="text-red-600 bg-red-50 px-1 text-sm"><?= $id_pays_err ?></p>
 
                     <label for="city-image" class="text-gray-600">Select an image</label>
-                    <input id="city-image" name="city-image" class="bg-gray-100 p-1" type="file">
+                    <input value="<?= $city_image ?>" id="city-image" name="city-image" class="bg-gray-100 p-1" type="file">
+                    <p id="city-image-err" class="text-red-600 bg-red-50 px-1 text-sm"><?= $city_image_err ?></p>
                 </div>
 
                 <div class="flex gap-1 flex-wrap-reverse">
